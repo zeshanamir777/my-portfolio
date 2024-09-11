e8bf8b0d1ae55a2167e123d52e929bb8803ff883
Jugnuu12 <zeeshan.amir500@gmail.com>
2024-09-11 17:08:41 +0500

    adding values

--- src/app/components/works-section/works-section.component.ts
+++ src/app/components/works-section/works-section.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-works-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './works-section.component.html',
   styleUrl: './works-section.component.scss'
 })
 export class WorksSectionComponent {
-
+  activeFilter: string = 'angular';
+  setActiveFilter(filter: string) {
+    this.activeFilter = filter;
+  }
 }
